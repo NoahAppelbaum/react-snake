202acc39bb60bac80206d485803afb53d40d1285
Noah Appelbaum <noahappelbaum@gmail.com>
2023-12-22 17:10:01 -0600

    set up useReducer call, added next step TODOs

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,1 +1,17 @@
 import { useReducer, useRef } from "react";
+import { stateReducer, createInitialState } from "./stateReducer";
+
+function Game({gameHeight = 101, gameWidth = 101}) {
+    const [state, dispatch] = useReducer(
+        stateReducer,
+        [gameHeight, gameWidth],
+        createInitialState
+    );
+
+
+    //Make some listeners for keystrokes
+    //  -On window? In a component? huh?
+    //  - Then, call dispatch with the appropriate `direction` property on `action`
+}
+
+export default Game;
